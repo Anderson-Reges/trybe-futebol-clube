113577257a6f478cf82563061346320fce6cd8d2
Anderson-Reges <andersonreges76@gmail.com>
2023-03-01 15:40:41 -0300

    Feat: adding test the route '/teams'

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -4,35 +4,38 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-
-import { Response } from 'superagent';
-import TeamService from '../services/TeamsService';
 import { teamsList } from './mocks/teams.mock';
+import Team from '../database/models/team.model';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('testa a rota "/teams"', () => {
-
-  let chaiHttpResponse: Response;
-
-  beforeEach(async () => {
-    sinon
-      .stub(new TeamService, 'findAll')
-      .resolves(teamsList);
+describe('check a route "/teams"', () => {
+  
+  it('test if list all teams', async () => {
+    const response = await chai
+      .request(app)
+      .get('/teams');
+    
+    sinon.stub(Team, 'findAll').resolves(teamsList as Team[])
+    
+    expect(response.status).to.be.equal(200)
+    expect(response.body).to.be.deep.equal(teamsList)
   });
+
+  it('test if list one team', async () => {
+    const response = await chai
+      .request(app)
+      .get('/teams/1');
+    
+    sinon.stub(Team, 'findByPk').resolves(teamsList[0] as Team)
+
+    expect(response.status).to.be.equal(200)
+    expect(response.body).to.be.deep.equal(teamsList[0])
+  })
 
   afterEach(()=>{
     sinon.restore();
   })
-
-  it('Testa a listagem de todos os times', async () => {
-    chaiHttpResponse = await chai
-       .request(app)
-       .get('/teams');
-
-    expect(chaiHttpResponse).to.have.property('status')
-    expect(chaiHttpResponse.status).to.be.equal(200)
-  });
 });
