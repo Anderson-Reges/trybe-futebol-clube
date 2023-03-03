ca8f4778b8dfc2e3fc4c3c1619e496adf468e54f
Anderson-Reges <andersonreges76@gmail.com>
2023-03-02 23:24:53 -0300

    feat: adding test the route '/matches/...'

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -4,6 +4,7 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
+import { Response } from 'superagent';
 
 chai.use(chaiHttp);
 
@@ -34,6 +35,120 @@ describe('checking the route "/matches"', () => {
       }) as Response
   })
 
+  it('test if is possible finish a match', async () => {
+    chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: "admin@admin.com", password: "secret_admin"})
+      const token = chaiHttpResponse.body.token;
+
+      chaiHttpResponse = await chai
+      .request(app)
+      .patch('/matches/48/finish')
+      .set('authorization', token)
+      .then((res) => {
+        expect(res.status).to.be.equal(200)
+        expect(res.body.message).to.be.equal('Finished')
+      }) as Response
+  })
+
+  it('test if is possible update a match', async () => {
+    chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: "admin@admin.com", password: "secret_admin"})
+      const token = chaiHttpResponse.body.token;
+
+    chaiHttpResponse = await chai
+      .request(app)
+      .patch('/matches/1')
+      .set('authorization', token)
+      .send({
+        homeTeamGoals: 3,
+        awayTeamGoals: 1
+      })
+      .then((res) => {
+        expect(res.status).to.be.equal(200)
+        expect(res.body.message).to.be.equal('Match updated with success')
+      }) as Response
+  })
+
+  it('test if is possible create a match', async () => {
+    chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: "admin@admin.com", password: "secret_admin"})
+      const token = chaiHttpResponse.body.token;
+
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/matches')
+      .set('authorization', token)
+      .send({
+        homeTeamId: 16,
+        awayTeamId: 8,
+        homeTeamGoals: 2,
+        awayTeamGoals: 2,
+      })
+      .then((res) => {
+        expect(res.status).to.be.equal(201)
+        expect(res.body).not.to.be.empty
+        expect(res.body).to.have.property('id')
+        expect(res.body).to.have.property('homeTeamId')
+        expect(res.body).to.have.property('awayTeamId')
+        expect(res.body).to.have.property('homeTeamGoals')
+        expect(res.body).to.have.property('awayTeamGoals')
+        expect(res.body).to.have.property('inProgress')
+      }) as Response
+  })
+
+  it('test if is possible create a match with teamId nonexistent', async () => {
+    chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: "admin@admin.com", password: "secret_admin"})
+      const token = chaiHttpResponse.body.token;
+    
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/matches')
+      .set('authorization', token)
+      .send({
+        "homeTeamId": 999,
+        "awayTeamId": 8,
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 2,
+      })
+      .then((res) => {
+        expect(res.status).to.be.equal(404)
+        expect(res.body).not.to.be.empty
+        expect(res.body.message).to.be.equal('There is no team with such id!')
+      }) as Response
+  })
+
+  it('test if is possible create a match with teamId identical', async () => {
+    chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: "admin@admin.com", password: "secret_admin"})
+      const token = chaiHttpResponse.body.token;
+
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/matches')
+      .set('authorization', token)
+      .send({
+        "homeTeamId": 8,
+        "awayTeamId": 8,
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 2,
+      })
+      .then((res) => {
+        expect(res.status).to.be.equal(422)
+        expect(res.body).not.to.be.empty
+        expect(res.body.message).to.be.equal('It is not possible to create a match with two equal teams')
+      }) as Response
+  })
 
   afterEach(()=>{
     sinon.restore();
